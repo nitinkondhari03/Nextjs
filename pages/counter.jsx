a36518f57ae68eab79e2ce8822c312062e7a50bd
Nitin Kondhari <nitinkondhari85@gamil.com>
2023-01-06 16:08:09 +0530

    add this file

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
 
+import React, { useState } from "react"
 const Counter = () => {
+  const [count,setcount]=useState(0)
   return (
-    <div>Counter</div>
+    <div>
+      <h1>Counter {count}</h1>
+      <button onClick={()=>setcount(count-1)}>DEC</button>
+      <button onClick={()=>setcount(count-1)}>INC</button>
+    </div>
   )
 }
 
